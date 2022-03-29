41c71a3b251b5f29c5c0efd712e374ee26b1cd34
Ashkins <102609965+Ashkins@users.noreply.github.com>
2022-03-29 19:22:09 +0300

    Add files via upload

---
+++ Scripts/FlPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FlPlayer : MonoBehaviour
+{
+    public Transform fl;
+
+    public Vector3 offset;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = fl.position + offset;
+    }
+}
---
+++ Scripts/PlayerColl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerColl : MonoBehaviour
+{
+    public PlMov movement;
+    
+
+
+   void OnCollisionEnter (Collision collisionInfo)
+    {
+       if (collisionInfo.collider.tag == "Enem")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+
+        }
+
+    }
+}
